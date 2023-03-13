90c6c075f1495187cfd4eeabd7125706f581a3a1
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-03-13 21:07:04 +0400

    add types

--- src/getMaintainerDownloads.js
+++ src/getMaintainerDownloads.js
@@ -1,22 +1,31 @@
 const NpmApi = require('npm-api');
 
+const { orderRecordStringNumber } = require('./orderRecordStringNumber');
+
 const npm = new NpmApi();
 
-const sumArray = (accumulator, currentValue) => accumulator + currentValue;
+const sumArray = (/** @type {number} */ accumulator, /** @type {number} */ currentValue) =>
+  accumulator + currentValue;
 
+/**
+ * @param {string} username
+ */
 async function getMaintainerDownloads(username) {
   const maintainer = npm.maintainer(username);
 
+  /** @type {string[]} */
   const repoNames = await maintainer.repos();
 
   let totalMaintainerDownloads = 0;
 
+  /** @type {Record<string, number>} */
   const reposWithDownloads = {};
 
   await Promise.all(
     repoNames.map(async (repoName) => {
       const repo = npm.repo(repoName);
 
+      /** @type { { downloads: number }[] } */
       const downloads = await repo.downloads();
 
       const onlyDownloads = downloads.map((el) => el.downloads);
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const getMaintainerDownloads = require('./src/getMaintainerDownloads');
 
 console.time();
+// assumes that your PC username is the same as your npm username.
 getMaintainerDownloads(process.env.USER).then((data) => {
   console.log(data);
   console.timeEnd();
